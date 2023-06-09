770f99fc546adf64ca9e38efcdf0ab3882416092
Maxillow <87654321thegroup@gmail.com>
2023-06-09 01:04:53 -0400

    Initial Push
    I put all the files in :+1:

---
+++ I just need a timer/Assets/timer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using System.Threading;
+
+public class timer : MonoBehaviour {
+
+    public TMP_Text aaa;
+    public TMP_Text words;
+    public AudioSource beep;
+    public float num = 59;
+    public bool toggle = true;
+
+    private void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            if (toggle)
+            {
+                while (num > 0)
+                {
+                    if (num >= 10)
+                    {
+                        aaa.text = "0:" + num.ToString();
+                    }
+                    else
+                    {
+                        aaa.text = "0:0" + num.ToString();
+                    }
+
+                    if (num < 5)
+                    {
+                        beep.Play();
+                    }
+
+                    yield return new WaitForSeconds(1f);
+                    num--;
+                }
+
+                words.text = "Break";
+                toggle = false;
+                num = 5;
+                aaa.text = "0:05";
+            }
+            else
+            {
+                while (num > 0)
+                {
+                    aaa.text = "0:0" + num.ToString();
+                    yield return new WaitForSeconds(1f);
+                    num--;
+                }
+
+                words.text = "Timer";
+                toggle = true;
+                num = 59;
+            }
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unity-Timer
- I just needed a custom timer that changed between 1 minute and 5 seconds and couldn't find a good one so I made onecond intervals
+I just needed a custom timer that changed between 1 minute and 5 second intervals and couldn't find a good one so I made one myself in Unity. There are probably several much easier ways to do this. Oh well.
+
+At least whoever else who needs this speciifc type of timer can use this as a base instead of starting from scratch. 
